e248165adb7991916b66aac43da4a4747cc3eb8f
mathieu.rolland14270@orange.fr <mathieu.rolland14270@orange.fr>
2016-06-18 19:33:55 +0200

    Package création

--- install/install.sh
+++ install/install.sh
@@ -4,12 +4,17 @@ INSTALLATION_DIR="/product/server/liferay/automatique_install"
 EXPORT_DIR="${WORKSPACE}"
 PACKAGE_NAME="LiferayWorkspace.tar.gz"
 
+echo "Entering direcory ${WORKSPACE}"
 cd "${WORKSPACE}"
-tar -zcvf "${PACKAGE_NAME}" "LiferayWorkspace/*"
+echo "Generate package ${PACKAGE_NAME}"
+tar -zcvf "${PACKAGE_NAME}" "LiferayWorkspace"
 
 mkdir -p "${INSTALLATION_DIR}"
-
+echo "Moving package ${PACKAGE_NAME} to ${INSTALLATION_DIR}"
 mv "${PACKAGE_NAME}" "${INSTALLATION_DIR}" 
 
+
+echo "Unpackage archive ${PACKAGE_NAME}"
 cd "${INSTALLATION_DIR}"
-tar -zxvf "${PACKAGE_NAME}"
+gunzip "${PACKAGE_NAME}"
+tar xvf "${PACKAGE_NAME}"
